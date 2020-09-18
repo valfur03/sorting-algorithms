71b1777195c43296b8d3eb36e1ecd121572e7f67
Valentin <valentin.furmanek@gmail.com>
2020-09-18 19:40:31 +0200

    [ADD] selection sort

---
+++ selection/main.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfurmane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/18 14:04:49 by vfurmane          #+#    #+#             */
+/*   Updated: 2020/09/18 18:39:27 by vfurmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+#include "put.h"
+
+void	ft_sort(int *tab, int size)
+{
+	int	i;
+	int	min;
+	int	tmp;
+	int	start;
+
+	start = 0;
+	while (start < size)
+	{
+		i = start;
+		min = start;
+		while (i < size)
+			if (tab[min] > tab[i++])
+				min = i - 1;
+		tmp = tab[min];
+		tab[min] = tab[start];
+		tab[start++] = tmp;
+	}
+}
+
+void	ft_aff_tab(int *tab, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		ft_putnbr(tab[i++]);
+		ft_putchar(1, '\n');
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int	size;
+	int	*numbers;
+
+	if (argc != 2)
+		return (0);
+	numbers = NULL;
+	size = ft_get_num(argv[1], &numbers);
+	if (!numbers)
+		return (0);
+	ft_sort(numbers, size);
+	ft_aff_tab(numbers, size);
+	free(numbers);
+	return (0);
+}
